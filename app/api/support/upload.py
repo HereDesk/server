23f4f1f3d9c03a1d7fe78951f1aa3722cea61e2d
xiaohutu <shanxi.1991@gmail.com>
2019-01-15 10:53:39 +0800

    优化 文件上传接口,并增加上传格式

--- app/api/support/upload.py
+++ app/api/support/upload.py
@@ -13,9 +13,11 @@ from app.models import User
 @csrf_exempt
 def upload(request):
 
-    allow_suffix_list = ["jpg","png","jpeg","gif","bmp",\
-        "docx","docx","xls","xlsx","ppt","pptx","pdf","txt","log","md","html","json",\
-        "mp4","mp3","mov"]
+    allow_suffix_list = [
+        "jpg","png","jpeg","gif","bmp","svg","psd","tif","tga","ai",\
+        "docx","docx","xls","xlsx","ppt","pptx","pdf","txt","log","md","html","json","ini","yaml",\
+        "mp4","mp3","mov","m4v","wmv","ts","3gp","avi","flv","mkv","mpeg",
+        "zip","rar","tar","7z","bz2","gz"]
 
     data_type = request.GET["type"]
     if data_type == "bug":
@@ -26,18 +28,18 @@ def upload(request):
         path = "media/other/"
     
     try:
-        original_filename = str(request.FILES.get('files'))
-        suffix = str(original_filename.split('.')[-1]).lower()
+        original_filename = str(request.FILES.get("files"))
+        suffix = str(original_filename.split(".")[-1]).lower()
         if suffix not in allow_suffix_list:
-            return JsonResponse({"status":20004,"msg":"{0}文件不被允许".format(suffix)})
+            return JsonResponse({"status":20004,"msg":"{0}文件不被允许, 若要上传, 请压缩后在上传！".format(suffix)})
     except Exception as e:
         return JsonResponse({"status":20004,"msg":"无法识别文件后缀"})
 
-    file_content = request.FILES.get('files')
+    file_content = request.FILES.get("files")
 
     try:
         filename = str(uuid.uuid4().hex) + "." + suffix
-        with open(path+filename, 'wb+') as destination:
+        with open(path+filename, "wb+") as destination:
             for chunk in file_content.chunks():
                 destination.write(chunk)
     except Exception as e:
