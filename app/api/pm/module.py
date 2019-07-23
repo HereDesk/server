23ad5779c719d2eca9ad5346262773c383ebce93
小糊涂 <>
2019-07-23 09:41:20 +0800

    优化 一级模块删除功能

--- app/api/pm/module.py
+++ app/api/pm/module.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse
 from django.http import HttpResponse
 from django.db.models import Q
 from django.db.models import F
+from django.db.models import Count
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
@@ -49,7 +50,7 @@ def module_list_all(request):
         return JsonResponse({"status":20000,"product_id":product_id,"product_code":product_data[0],"data":data})
 
 """
-  模块：增加
+  模块：增加/编辑
 """
 @csrf_exempt
 @require_http_methods(["POST"])
@@ -61,11 +62,11 @@ def module_edit_a(request):
         m1_name = rep["m1_name"]
     except Exception as e:
         return JsonResponse({"status":40001,"msg":"模块名称是必填项哦"})
-    
+
     if len(m1_name) > 20:
         return JsonResponse({"status":20004,"msg":"名称长度的合理范围为2到20位"})
 
-    n = ModuleA.objects.filter(Q(m1_name=m1_name) & Q(product_id=product_id)).count()
+    n = ModuleA.objects.filter(Q(m1_name=m1_name) & Q(product_id=product_id)  & Q(is_delete=0)).count()
     if n > 0:
         return JsonResponse({"status":20004,"msg":"名称已存在，请勿重复添加"})
 
@@ -78,7 +79,7 @@ def module_edit_a(request):
             return JsonResponse({"status":20004,"msg":"修改失败"})
         else:
             return JsonResponse({"status":20000,"msg":"修改成功"})
-        
+
     else:
         try:
             # 保存module
@@ -97,40 +98,37 @@ def module_edit_a(request):
 
 
 """
-  二级模块：增加
+  一级模块：删除
 """
 @csrf_exempt
-@require_http_methods(["POST"])
-def module_add_b(request):
+@require_http_methods(["GET"])
+def module_del_a(request):
 
     try:
-        rep = json.loads(request.body)
-        m1_id = rep["m1_id"]
-        m2_name = rep["m2_name"]
+        m1_id = request.GET["m1_id"]
     except Exception as e:
-        print(e)
-        return JsonResponse({"status":40001,"msg":"module信息是必填项哦"})
-
-    if len(m2_name) > 20:
-        return JsonResponse({"status":20004,"msg":"名称长度的合理范围为2到20位"})
-
-    n = ModuleB.objects.filter(Q(m1_id=m1_id) & Q(m2_name=m2_name)).count()
-    if n > 0:
-        return JsonResponse({"status":20004,"msg":"此名称已存在，请勿重复添加"})
+        return JsonResponse({"status":40001,"msg":"缺少必填参数"})
 
     try:
-        # 保存module
-        m = ModuleB(
-            m1_id=ModuleA.objects.get(m1_id=m1_id),
-            m2_name=m2_name,
-            creator_id=get_user_object(request)
-            )
-        m.save()
+        m1_obj = ModuleA.objects.get(m1_id=m1_id)
     except Exception as e:
-        print(e)
-        return JsonResponse({"status":20004,"msg":"保存失败"})
+        return JsonResponse({"status":20004,"msg":"无效的ID"})
+
+    # 检查是否存在二级模块
+    exist_num = ModuleB.objects.filter(Q(m1_id=m1_id) & Q(is_delete=0)).count()
+    if exist_num != 0:
+        return JsonResponse({"status":20004,"msg":"该模块下，存在二级模块，请先删除二级模块"})
+
+    try:
+        m1_obj.is_delete = 1
+        m1_obj.deleter_id = get_user_object(request)
+        m1_obj.delete_time = curremt_time
+        m1_obj.save()
+    except Exception as e:
+        return JsonResponse({"status":20004,"msg":"删除失败"})
     else:
-        return JsonResponse({"status":20000,"msg":"保存成功"})
+        return JsonResponse({"status":20000,"msg":"删除成功"})
+
 
 """
   二级模块：编辑
@@ -141,36 +139,57 @@ def module_edit_b(request):
 
     try:
         rep = json.loads(request.body)
-        m2_id = rep["m2_id"]
         m2_name = rep["m2_name"]
     except Exception as e:
         print(e)
-        return JsonResponse({"status":40001,"msg":"缺少必填参数"})
-    else:
-        if len(m2_name) > 20:
-            return JsonResponse({"status":20004,"msg":"名称长度的合理范围为2到20位"})
+        return JsonResponse({"status":40001,"msg":"二级模块名称为必填项"})
 
-    try:
-        m2_obj = ModuleB.objects.get(m2_id=m2_id)
-    except Exception as e:
-        return JsonResponse({"status":40001,"msg":"该条记录不存在"})
+    if len(m2_name) > 20:
+        return JsonResponse({"status":20004,"msg":"名称长度的合理范围为2到20位"})
 
-    query_data = ModuleB.objects.filter(Q(m2_id=m2_id) & Q(m2_name=m2_name)).values("m2_name")
-    if len(query_data) > 0:
-        if list(query_data)[0]["m2_name"] == m2_name:
-            return JsonResponse({"status":20004,"msg":"新提交的模块名称与旧模块名称相同"})
+    if "m2_id" in rep and "m1_id" not in rep:
+        try:
+            m2_id = rep["m2_id"]
+            m2_obj = ModuleB.objects.get(m2_id=m2_id)
+        except Exception as e:
+            return JsonResponse({"status":40001,"msg":"id获取错误"})
 
-    try:
-        m2_obj.m2_name=m2_name
-        m2_obj.is_change = 1
-        m2_obj.changer_id = get_user_object(request)
-        m2_obj.change_time = curremt_time
-        m2_obj.save()
-    except Exception as e:
-        print(e)
-        return JsonResponse({"status":20004,"msg":"二级模块修改失败"})
-    else:
-        return JsonResponse({"status":20000,"msg":"二级模块修改成功"})
+        try:
+            m2_obj.m2_name=m2_name
+            m2_obj.is_change = 1
+            m2_obj.changer_id = get_user_object(request)
+            m2_obj.change_time = curremt_time
+            m2_obj.save()
+        except Exception as e:
+            print(e)
+            return JsonResponse({"status":20004,"msg":"二级模块修改失败"})
+        else:
+            return JsonResponse({"status":20000,"msg":"二级模块修改成功"})
+
+    if "m1_id" in rep:
+        try:
+            m1_id = rep["m1_id"]
+            ModuleA.objects.get(m1_id=m1_id)
+        except Exception as e:
+            return JsonResponse({"status":40001,"msg":"id获取错误"})
+
+        is_exist = ModuleB.objects.filter(Q(m1_id=m1_id) & Q(m2_name=m2_name) & Q(is_delete=0)).count()
+        if is_exist > 0:
+            return JsonResponse({"status":20004,"msg":"此名称已存在，请勿重复添加"})
+
+        try:
+            m = ModuleB(
+                m1_id=ModuleA.objects.get(m1_id=m1_id),
+                m2_name=m2_name,
+                creator_id=get_user_object(request)
+                )
+            m.save()
+        except Exception as e:
+            print(e)
+            return JsonResponse({"status":20004,"msg":"保存失败"})
+        else:
+            return JsonResponse({"status":20000,"msg":"保存成功"})
+
 
 
 """
